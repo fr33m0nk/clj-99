f743b1d069923d75546a3f378177c0214a9c2c51
fr33m0nk <prashantsinha@protonmail.com>
2020-07-05 18:57:57 +0530

    [fr33m0nk]: Adds ninth problem: Pack consecutive duplicates of list elements into sublists.

---
+++ src/clj_99/list/problem_09.clj
@@ -0,0 +1,30 @@
+(ns clj-99.list.problem-09)
+
+;Pack consecutive duplicates of list elements into sublists.
+;If a list contains repeated elements they should be placed in separate sublists.
+
+(defn pack-1
+  [ls]
+  (-> (loop [ls ls
+             acc '()
+             prev nil]
+        (if (empty? ls)
+          acc
+          (let [[head & tail] ls]
+            (if (and (seq? (first acc)) (= head prev))
+              (recur tail (cons (cons head (first acc)) (rest acc)) head)
+              (recur tail (cons (list head) acc) head)))))
+      reverse))
+
+(defn pack-2
+  [ls]
+  (-> (reduce (fn [acc x]
+                (if (and (seq? (first acc)) (= (first (first acc)) x))
+                  (cons (cons x (first acc)) (rest acc))
+                  (cons (list x) acc))) () ls)
+      reverse))
+
+(defn pack-3
+  [ls]
+  (partition-by identity ls))
+
